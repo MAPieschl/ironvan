01687f714a18904dfa2d5e46eb3bd388446b9aab
Mike Pieschl <mapieschl@gmail.com>
2024-02-29 21:56:28 -0600

    Ignoring venv

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m pip install --upgrade pip setuptools virtualenv
 python3 -m venv kivy_venv
 source kivy_venv/bin/activate
-python3 -m pip3 install "kivy[base]"
+python3 -m pip install "kivy[base]"
