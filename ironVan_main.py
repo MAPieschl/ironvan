f241ffb78350b1f94adb32155b93cf091a01e711
Mike Pieschl <mapieschl@gmail.com>
2024-03-08 00:45:48 -0600

    Incremental Changes

--- ironVan_main.py
+++ ironVan_main.py
@@ -1,6 +1,7 @@
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.animation import Animation
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from kivymd.app import MDApp
 
@@ -24,10 +25,28 @@ class appElementIDs():
 			'nav_button_water',
 			'nav_button_lights'
 		]
-		self.cards = []
+		self.cards = [
+			'weather_quick_card',
+			'thermostat_quick_card',
+			'bemu_quick_card',
+			'ws_quick_card',
+			'ls_quick_card',
+			'bemu_home_card'
+		]
+
+class BEMUHomeScreen(Screen):
+	pass
+
+class AppHomeScreen(Screen):
+	pass
+
+class PageManager(ScreenManager):
+	pass
 
 class ironVanApp(MDApp):
+
 	appIDs = appElementIDs()
+
 	def build(self):
 
 		# ---- Build Window ----
@@ -66,18 +85,21 @@ class ironVanApp(MDApp):
 				self.theme_cls.bg_darkest if self.theme_cls.theme_style == 'Light' else self.theme_cls.bg_light
 			)
 		for id in self.appIDs.buttons:
-			print(id)
 			self.root.ids[id].md_bg_color = (
 				self.theme_cls.primary_light if self.theme_cls.theme_style == 'Light' else self.theme_cls.primary_dark
+			)
+		for id in self.appIDs.cards:
+			self.root.ids[id].md_bg_color = (
+				self.theme_cls.bg_darkest if self.theme_cls.theme_style == 'Light' else self.theme_cls.bg_light
 			)
 
 	def navButtonRouter(self, id):
 
+		# Copy list of navigation buttons to identify !ids
 		otherButtons = self.appIDs.buttons[:]
-		print(self.appIDs.buttons)
 		otherButtons.remove(id)
-		print(self.appIDs.buttons)
 
+		# Create animation for the active button (pop-out)
 		animateButtonActive = Animation(
 			pos_hint = {'center_x': 0.8},
 			duration = 0.003
@@ -95,6 +117,7 @@ class ironVanApp(MDApp):
 			duration = 0.012
 		)
 
+		# Create animation for the retreating button (slide-in)
 		animateButtonRetreat = Animation(
 			pos_hint = {'center_x': 0.95},
 			duration = 0.003
@@ -112,11 +135,21 @@ class ironVanApp(MDApp):
 			duration = 0.012
 		)
 		
+		# Call animations to pop-out or slide-in buttons
 		animateButtonActive.start(self.root.ids[id])
 
+		lastPage = None
 		for otherID in otherButtons:
 			if(self.root.ids[otherID].pos_hint['center_x'] != 0.7):
 				animateButtonRetreat.start(self.root.ids[otherID])
-
+				lastPage = otherID
+				
+		match id:
+			case 'nav_button_home':
+				self.root.ids.page_manager.current = 'app_home_page'
+			
+			case 'nav_button_electricity':
+				self.root.ids.page_manager.current = 'bemu_home_page'
+		
 
 ironVanApp().run()
